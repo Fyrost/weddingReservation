9813896612d74a548706966898c73c10b40ea01f
ryantesoro <ryantesoro@yahoo.com>
2017-12-17 13:59:20 +0800

    asd

--- index.php
+++ index.php
@@ -1,17 +1,2 @@
-<<<<<<< HEAD
-<?php
-	echo "shiiiiiiiiit";
-       echo "something";
-			 echo "123";
-			 echo "asdayoo";
-                      echo "shit";
-                      echo "ano?";
-                      echo "yo";
-											echo "asd?";
-                      echo "asd?";
-                      echo "yes";
-                      echo "no";
-?>
-=======
 545465465465465
->>>>>>> 2e3d5b1242735f4e9bb97f331078e3c3482ecd69
+1 ryan
