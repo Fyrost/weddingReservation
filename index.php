428b546b70f6612af98536abe1c9a8152d3d1505
ryantesoro <ryantesoro@yahoo.com>
2017-12-17 19:56:40 +0800

    bootstrap

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
-545465465465465
-<<<<<<< HEAD
-1 ryan
-=======
->>>>>>> 1c831892bc0a784387544e3b08fab93f6c6c390c
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	</head>
+	<body>
+
+	</body>
+</html>
