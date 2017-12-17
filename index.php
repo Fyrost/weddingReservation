d0ea986b424c7d934e38e0808a9d292faccafd97
ryantesoro <ryantesoro@yahoo.com>
2017-12-17 23:51:53 +0800

    asd

--- index.php
+++ index.php
@@ -24,6 +24,44 @@
          </div>
       </div>
     </nav>
+		<div id="myCarousel" class="carousel slide">
+  <!-- Indicators -->
+	    <ol class="carousel-indicators">
+	      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+	      <li data-target="#myCarousel" data-slide-to="1"></li>
+	      <li data-target="#myCarousel" data-slide-to="2"></li>
+	    </ol>
+
+	    <!-- Wrapper for slides -->
+	    <div class="carousel-inner">
+	      <div class="item active">
+	        <div class="fill" style="background-image:url('img/1.jpg');"></div>
+	        <div class="carousel-caption">
+	          <h1>Modern Business - A Bootstrap 3 Template</h1>
+	        </div>
+	      </div>
+	      <div class="item">
+	        <div class="fill" style="background-image:url('img/2.jpg');"></div>
+	        <div class="carousel-caption">
+	          <h1>Ready to Style &amp; Add Content</h1>
+	        </div>
+	      </div>
+	      <div class="item">
+	        <div class="fill" style="background-image:url('img/3.jpg');"></div>
+	        <div class="carousel-caption">
+						asd
+	        </div>
+	      </div>
+	    </div>
+
+	    <!-- Controls -->
+	    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+	      <span class="icon-prev"></span>
+	    </a>
+	    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+	      <span class="icon-next"></span>
+	    </a>
+	</div>
     <div class="container-fluid bg-sample" style=""></div>
     <div style="height:10000px;"></div>
 		<script src="js/jquery.min.js"></script>
