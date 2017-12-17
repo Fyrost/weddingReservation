bc64c9d06331e7175a1a18a86b1c68cabe25abb8
ryantesoro <ryantesoro@yahoo.com>
2017-12-17 20:06:56 +0800

    Yeboy

--- index.php
+++ index.php
@@ -1,10 +1,34 @@
-<!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/css.css" rel="stylesheet">
 		<title></title>
 	</head>
 	<body>
-
+		<nav class="nav navbar-default" style="background-color:#dff7ff;">
+      <div class="container">
+        <div class="navbar-header">
+          <div class="navbar-brand text-primary">
+            <span class="glyphicon glyphicon-plus"></span>
+            Clinic
+          </div>
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#options">
+               <span class="icon-bar"></span>
+               <span class="icon-bar"></span>
+               <span class="icon-bar"></span>
+           </button>
+        </div>
+         <div class="collapse navbar-collapse navbar-left" id="options">
+            <ul class="nav navbar-nav">
+               <li><a href="index.php"><b> Home </b></a></li>
+               <li><a href="#"><b> Profile </b></a></li>
+            </ul>
+         </div>
+      </div>
+    </nav>
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/js.js"></script>
 	</body>
 </html>
