c30c0a52285bbe1a41ed29e3a2088a03e7cd85ee
Fyrost <fmga.webbie@gmail.com>
2017-12-18 20:01:39 +0800

    change db to reservation

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
          $hostname = 'localhost';
          $username = 'root';
          $password = '';
-         $dbname = 'students';
+         $dbname = 'reservation';
 
          //connect host
          $link = mysql_connect($hostname,$username,$password)
