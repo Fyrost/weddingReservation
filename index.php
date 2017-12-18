29f66027585b7bf9994b7ca811fd6e54d07929a9
Fyrost <fmga.webbie@gmail.com>
2017-12-18 18:28:51 +0800

    added datepicker

--- index.php
+++ index.php
@@ -69,16 +69,90 @@
            <div class="col-md-6 col-md-offset-3 text-center " >
 		    <div class="vector-w-head"></div>
                      <h2>Reserve a Date?</h2>
-                     <form method="post"  class="form-limit form">
-	                     <input class="form-control" type="text" name="name" placeholder="Name" required autofocus><br>
-	                     <input class="form-control" type="text" name="email" placeholder="E-mail" required><br>
-	                     <input class="btn btn-warning" type="submit" name="login">
-               	</form>
+			<div class="bootstrap-iso">
+                      <div class="container-fluid">
+                       <div class="row">
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                         <form>
+                          <div class="form-group ">
+                           <label class="control-label " for="name">
+                            Name
+                           </label>
+                           <input class="form-control" id="name" name="name" type="text"/>
+                          </div>
+                          <div class="form-group ">
+                           <label class="control-label requiredField" for="email">
+                            Email
+                            <span class="asteriskField">
+                             *
+                            </span>
+                           </label>
+                           <input class="form-control" id="email" name="email" type="text"/>
+                          </div>
+                          <div class="form-group ">
+                           <label class="control-label " for="number">
+                            Number
+                           </label>
+                           <input class="form-control" id="number" name="number" type="text"/>
+                          </div>
+<span id="dateWarning"></span>
+                          <div class="form-group ">
+                           <label class="control-label requiredField" for="date">
+                            Date
+                            <span class="asteriskField">
+                             *
+                            </span>
+                           </label>
+                           <div class="input-group">
+
+                            <div class="input-group-addon">
+                             <i class="fa fa-calendar">
+                             </i>
+                            </div>
+                            <input class="form-control" id="date" name="date" placeholder="MM/DD/YYYY" type="text"/>
+                           </div>
+                          </div>
+                          <div class="form-group">
+
+                           <div>
+                            <button class="btn btn-primary " name="submit" type="submit">
+                             Submit
+                            </button>
+                           </div>
+                          </div>
+                         </form>
+                        </div>
+                       </div>
+                      </div>
+                     </div>
 			<div class="vector-w-foot"></div>
            </div>
     </div>
 		<script src="js/jquery.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
 		<script src="js/js.js"></script>
+		<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
+
+<script>
+    $(function(){
+        var date_input=$('#date'); //our date input has the name "date"
+        var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+        date_input.datepicker({
+               onSelect: function(){
+                      var day1 = $("#date").datepicker('getDate');
+                      document.getElementById("dateWarning").innerHTML = day1;
+               },
+            format: 'mm/dd/yyyy',
+            container: container,
+            todayHighlight: true,
+            autoclose: true
+        })
+    });
+
+
+
+</script>
 	</body>
 </html>
